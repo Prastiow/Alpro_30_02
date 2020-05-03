ccd0c5417abfc3072e1f2efeffda0fa573ad0d68
Tri Prastio Nugroho <Prasteow@users.noreply.github.com>
2020-05-03 07:59:41 +0700

    Commit Pertama

---
+++ src/ch02/ObjectMahasiswa.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class ObjectMahasiswa {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.nim = "10201";
+        mhs.nama = "Bayu";
+        mhs.alamat = "Jl.Kubang Raya No.321";
+        mhs.hp = "08216535734";
+        mhs.cetak();
+    
+    }
+
+}
